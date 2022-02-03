d179e810d17caffc31bde17e9497988b9e76cb5b
AllWKA <bryanjaramillobaldeon@gmail.com>
2022-02-03 20:57:54 +0000

    login as app

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
 console.log('banana');
+const url = 'https://api.twitter.com/oauth2/token?grant_type=client_credentials';
+axios_1.default.post(url, {}, {
+    auth: {
+        username: 'bryanjbj1@gmail.com',
+        password: '1996jb6666'
+    }
+})
+    .then(res => {
+    console.log(res.data);
+})
+    .catch(e => {
+    console.log(e.message);
+});
+// auth: {
+//     username: '5cVNfElXwznnVm2UMrVmqXAQ9',
+//         password: 'PlB5qwFrAzrjbzbwH6AZ76wieqNuOEuMeEFptLzYtr1ZgbbnWr'
+// }
--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
+import axios from 'axios'
+
 console.log('banana')
+
+const url = 'https://api.twitter.com/oauth2/token?grant_type=client_credentials'
+
+axios.post(url,{},
+    {
+        auth: {
+            username: '',
+            password: ''
+        }
+    }
+)
+    .then(res => {
+        console.log(res.data)
+    })
+    .catch(e => {
+        console.log(e.message)
+    })
